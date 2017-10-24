98fb4b44a0a3ab88420e002b59d819737ac42435
sebspas <sebspas@gmail.com>
2017-10-24 18:37:20 -0400

    First auth system

--- API/main.py
+++ API/main.py
@@ -1,18 +1,20 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template, redirect, url_for, request
+
 app = Flask(__name__)
 
-
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+# Go to login page
+@app.route('/', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            return redirect(url_for('hello'))
+    return render_template('login.html', error=error)
 
 
 @app.route('/hello/')
 @app.route('/hello/<name>')
 def hello(name=None):
     return render_template('hello.html', name=name)
-
-
-if __name__ == "__main__":
-    app.run()
