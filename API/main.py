8c82e14719c844d6e97be642e6e25785b1269f3a
sebspas <sebspas@gmail.com>
2017-10-24 19:20:29 -0400

    Pass with db full working, username=admin and pwd=admin

--- API/main.py
+++ API/main.py
@@ -1,20 +1,59 @@
-from flask import Flask, render_template, redirect, url_for, request
+from flask import Flask, render_template, redirect, url_for, request, session
+import sqlite3, hashlib, os
 
 app = Flask(__name__)
+app.secret_key = os.urandom(12)
 
 # Go to login page
 @app.route('/', methods=['GET', 'POST'])
 def login():
+    #already logged ?
+    if session.get('logged_in'):
+        return redirect(url_for('hello'))
     error = None
     if request.method == 'POST':
-        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
-            error = 'Invalid Credentials. Please try again.'
+        username = request.form['username']
+        password = request.form['password']
+        completion = validate(username, password)
+        if completion ==False:
+            error = 'Identifiants incorrect, merci de ressayer.'
         else:
+            session['logged_in'] = True
             return redirect(url_for('hello'))
     return render_template('login.html', error=error)
 
+@app.route('/logout')
+def logout():
+    if not session.get('logged_in'):
+        return redirect(url_for('login'))
+    else:
+        session['logged_in'] = False
+        return redirect(url_for('login'))
 
 @app.route('/hello/')
 @app.route('/hello/<name>')
 def hello(name=None):
-    return render_template('hello.html', name=name)
+    if not session.get('logged_in'):
+        return redirect(url_for('login'))
+    else:
+        return render_template('hello.html', name=name)
+
+
+#TODO change this into an other file
+# CHECK the user imput with the db value
+def validate(username, password):
+    con = sqlite3.connect('static/user.db')
+    completion = False
+    with con:
+        cur = con.cursor()
+        cur.execute("SELECT * FROM Users")
+        rows = cur.fetchall()
+        for row in rows:
+            dbUser = row[0]
+            dbPass = row[1]
+            if dbUser==username:
+                completion=check_password(dbPass, password)
+    return completion
+
+def check_password(hashed_password, user_password):
+    return hashed_password == hashlib.md5(user_password.encode()).hexdigest()
